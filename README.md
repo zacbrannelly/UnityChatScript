ed1832f78de68fd3cb6c9e9f20d9a72ac6cbc6a1
Zac Brannelly <101668878@student.swin.edu.au>
2019-02-07 11:22:49 +1100

    Upload plugin

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Unity ChatScript plugin
+# Unity ChatScript Plugin
 Native plugin for Unity to use ChatScript 
+
+## Requirements
+- Only built to work on Windows 64bit
+- Unity 2017+
+
+## How to import
+- Copy the `Plugins` & `StreamingAssets` folder into your assets folder
+- Now check that you can use the `UnityChatScript` namespace in a script
+- If that works you are good to go!
+
+## How to use 
+- Create an empty gameobject and add the `ChatScript` component. 
+- Then create and add a new script called `TestComponent` and do the following: 
+```csharp
+using UnityChatScript;
+
+public class TestComponent : MonoBehaviour
+{
+	public void Start() 
+	{
+		var chatScript = GetComponent<ChatScript>();
+		
+		Debug.Log(chatScript.Query("Hello there", "Guest", "Harry"));
+	}
+}
+```
+- This should show a response in the console. 
+- To create new bots, have a separate ChatScript installation, build your bot in there and copy the `TOPICS` & `RAWDATA` folder across to `StreamingAssets/chatscript/`
---
+++ UnityChatScript/UnityChatScript/ChatScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace UnityChatScript
+{
+    public class ChatScriptAPI
+    {
+        [DllImport("chatscript")]
+        public static extern uint chatscript_init_sys(int argc, string[] argv, string unchangedPath, string readonlyPath, string writablePath);
+
+        [DllImport("chatscript")]
+        public static extern void chatscript_perform_chat(string user, string usee, string incoming, string ip, [MarshalAs(UnmanagedType.LPStr)]StringBuilder buffer);
+
+        [DllImport("chatscript")]
+        public static extern void chatscript_close();
+    }
+
+    public class ChatScript : MonoBehaviour
+    {
+        private void Start()
+        {
+            var path = Application.streamingAssetsPath + "/chatscript";
+            if (ChatScriptAPI.chatscript_init_sys(0, new string[] { }, path, path, path) != 0)
+            {
+                Debug.LogError("Failed to initialize chatscript system!");
+            }
+        }
+
+        public string Query(string text, string username, string botName)
+        {
+            StringBuilder buffer = new StringBuilder(1024);
+            ChatScriptAPI.chatscript_perform_chat(username, botName, text, "0.0.0.0", buffer);
+
+            return buffer.ToString();
+        }
+    }
+}
